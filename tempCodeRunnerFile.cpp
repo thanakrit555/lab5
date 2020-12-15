90dfb76926ba9c424915e373dd637f20ca13e0c9
lab302-29 <lab302-29@labeng.local>
2020-12-15 16:59:16 +0700

    lab5_2.cpp completed

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include<cmath>
+
+using namespace std;
+
+double deg2rad( double x){
+     double s = x*(M_PI/180);
+     return s;
+}
+
+double rad2deg(double y){
+     double b = y*(180/M_PI);
+     return b;  
+}
+
+double  findXComponent(double l1,double l2,double a1,double a2){
+  double X= (l1*cos(a1))+(l2*cos(a2));
+  return X;
+}
+ double findYComponent(double l1,double l2,double a1,double a2){
+    double Y= (l1*sin(a1))+(l2*sin(a2));
+    return Y;
+ }
+
+ double pythagoras(double X,double Y){
+   double Z =sqrt((pow(X,2))+(pow(Y,2)));
+   return Z;
+ }
+
+double showResult( double result_vec_length,double result_vec_direction){
+cout<<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<endl;
+cout<<"Length of the resultant vector = "<<result_vec_length<<endl;
+cout<<"Direction of the resultant vector (deg) = "<<result_vec_direction<<endl;
+cout<<"%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%"<<endl;
+
+}
+
+
+int main(){
+    double l1,l2,a1,a2,xcomp,ycomp,result_vec_length,result_vec_direction;
+    cout << "Enter length of the first vector: ";
+    cin >> l1;
+    cout << "Enter direction of the first vector (deg): ";
+    cin >> a1;
+    cout << "Enter length of the second vector: ";
+    cin >> l2;
+    cout << "Enter direction of the second vector (deg): ";
+    cin >> a2;
+    
+    a1 = deg2rad(a1);
+    a2 = deg2rad(a2);
+    xcomp = findXComponent(l1,l2,a1,a2);
+    ycomp = findYComponent(l1,l2,a1,a2);
+    result_vec_length = pythagoras(xcomp,ycomp);
+    result_vec_direction = rad2deg(atan2(ycomp,xcomp)); 
+
+    showResult(result_vec_length,result_vec_direction);
+}
